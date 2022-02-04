2960bb40341439795e7d231b9b4ba5ea97521e40
sakilk130 <sakilk130@gmail.com>
2022-02-04 23:39:07 +0600

    feat: task completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+export default function App() {
+  const [input, setInput] = useState('');
+  const [result, setResult] = useState(0);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    setResult(factorialCalculation(input));
+  };
+
+  const factorialCalculation = (number) => {
+    if (number === 0 || number === 1) {
+      return 1;
+    } else {
+      return number * factorialCalculation(number - 1);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Factorial Calculator</h1>
+      <form onSubmit={submitHandler}>
+        <input
+          type="number"
+          placeholder="Enter a number..."
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <br />
+        <button type="submit">Calculate Factorial</button>
+      </form>
+      <h2>Factorial: {result}</h2>
     </div>
   );
 }
-
-export default App;
